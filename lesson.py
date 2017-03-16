aee32641b8cc13d1fdb48993a13abe80dd9271c1
wcomaqsw <544074672@qq.com>
2017-03-16 11:32:09 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -219,6 +219,6 @@ if __name__ == "__main__":
                 clas["done"] = True
             if not firsttime:
                 time.sleep(sleeptime)
-        flagtime = False
+        firsttime = False
 
     logging.info("已完成所有工作...\n好好学习，天天向上！！！")
